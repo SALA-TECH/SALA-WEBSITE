2fb74e303e006c99b79b145ca7e365d997a4a68c
LWANDO NKENJANA <142931452+nkenjana@users.noreply.github.com>
2026-03-29 02:33:12 +0000

    Updated App.tsx with various routes added
    - Added all new page routes
    - Configured React Router for proper navigation
    - Set up layout wrapper for consistent structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,34 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Contact from "./pages/Contact";
 import "./App.css";
 
+// TODO: create these pages when ready
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Media from "./pages/Media";
+import Library from "./pages/Library";
+import News from "./pages/News";
 
 function App() {
   return (
-    <>
-     <div className="App">
-      <div className="page-content">
-        <Home></Home>
-      </div>
-    </div>
-    </>
-   
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/media" element={<Media />} />
+          <Route path="/library" element={<Library />} />
+          <Route path="/news" element={<News />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
